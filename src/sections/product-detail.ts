df96a8f7b00226e636f996adc967edcaf7fcb3e6
Edward Lim Kok Siong <edwardlimkoksiong1994@gmail.com>
2023-06-23 10:16:42 +0800

    Compare at price

--- src/sections/product-detail.ts
+++ src/sections/product-detail.ts
@@ -3,6 +3,7 @@ import { createApp, reactive, ref } from "vue/dist/vue.esm-bundler";
 import { appConfig } from "../config";
 import { breakpoint } from "@/helper";
 import { LG } from "@/constants/breakpoint";
+import Cart from "@/plugins/Cart";
 
 export default function productDetailApp() {
 	return createApp({
@@ -14,17 +15,23 @@ export default function productDetailApp() {
 				sku: null,
 				title: null,
 				price: null,
+				compare_at_price: null,
 				max: null,
 			});
 
-			function selectVariant(id, title, price, qty) {
+			const qty = ref(1);
+
+			function selectVariant(id, title, price, compare_at_price, qty) {
 				chosenVariant.id = id;
 				chosenVariant.title = title;
 				chosenVariant.price = price;
+				chosenVariant.compare_at_price = compare_at_price;
 				chosenVariant.max = qty;
 			}
 
-			return { isDesktop, chosenVariant, selectVariant };
+			return { isDesktop, chosenVariant, selectVariant, qty };
 		},
-	}).mount("#product-detail");
+	})
+		.use(Cart)
+		.mount("#product-detail");
 }
